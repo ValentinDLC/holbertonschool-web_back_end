0e8b3cfa8243e051b863c3f5f0f08341ff4d7719
ValentinDLC <valentin.dardenne9@gmail.com>
2025-11-24 15:51:34 +0100

    coroutine yielding 10 random floats asynchronously

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+from typing import AsyncGenerator
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Yield 10 random floats asynchronously, waiting 1 second between each."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)

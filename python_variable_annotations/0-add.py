a15e4d8350956a4768651fc2610002730769d723
ValentinDLC <valentin.dardenne9@gmail.com>
2025-11-28 16:07:43 +0100

    fix problem

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
+"""Module that adds two float numbers"""
+
+
 def add(a: float, b: float) -> float:
+    """Add two floats and return their sum
+    
+    Args:
+        a: first float number
+        b: second float number
+    
+    Returns:
+        The sum of a and b as a float
+    """
     return a + b

a25fc52de5deccfe84383408305f67c8226c16b4
ValentinDLC <valentin.dardenne9@gmail.com>
2025-12-12 22:13:13 +0100

    implement Car class with cloneCar method

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+const cloneSymbol = Symbol('clone');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const Cls = this.constructor;
+    return new Cls(this._brand, this._motor, this._color);
+  }
+}
